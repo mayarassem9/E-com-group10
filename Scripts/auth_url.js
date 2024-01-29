503352fde6c5cfa483e2f9c0e7c062eb8ea878e8
Mohamed Hamed <Mohamedhamed3343@gmail.com>
2024-01-29 19:30:57 +0200

    kkk

--- Scripts/auth_url.js
+++ Scripts/auth_url.js
@@ -1,19 +1,15 @@
 let current = localStorage.getItem("currentUser");
-current = current ? JSON.parse(current):false;
-
-
-
-if(current){
-    let isseller=current.some((current) => current.role === "seller");
-    let isadmin=current.some((current) => current.role === "admin");
-    if(!isadmin){
-        window.location.href="index.html";
-    }
-    if(!isseller){
-        window.location.href="index.html";
-    }
-
-}
-else{
-    window.location.href="index.html";
+current = current ? JSON.parse(current) : false;
+debugger;
+if (current) {
+  let isseller = current.some((current) => current.role === "seller");
+  let isadmin = current.some((current) => current.role === "admin");
+  if (!isadmin) {
+    window.location.href = "../index.html";
+  }
+  if (!isseller) {
+    window.location.href = "index.html";
+  }
+} else {
+  window.location.href = "index.html";
 }
