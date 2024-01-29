379bb68e248eeced1b3e4987f412c96d2c8f9953
Mohamed Hamed <Mohamedhamed3343@gmail.com>
2024-01-29 19:40:19 +0200

    lllk

--- Scripts/logout.js
+++ Scripts/logout.js
@@ -2,12 +2,10 @@ const logout = document.getElementById("logout");
 const signup = document.getElementById("signup");
 const login = document.getElementById("login");
 const myaccount = document.getElementById("myaccount");
-const sellerdash=document.getElementById("sellerdashborad");
-const admindash=document.getElementById("admindashboard");
+const sellerdash = document.getElementById("sellerdashborad");
+const admindash = document.getElementById("admindashboard");
 
 document.addEventListener("DOMContentLoaded", function () {
-
-  
   // Check if the user has signed up or logined
   const userSignedUp = localStorage.getItem("userSignedUp");
 
@@ -22,30 +20,24 @@ document.addEventListener("DOMContentLoaded", function () {
     myaccount.style.display = "none";
   }
   dashboard();
-
 });
 logout.addEventListener("click", function () {
   localStorage.setItem("userSignedUp", "false");
   localStorage.removeItem("currentUser");
   window.location.href = "login.html";
 });
-function dashboard(){
-  admindash.style.display="none";
-  sellerdash.style.display="none";
-  let currentuser=localStorage.getItem("currentUser")
+function dashboard() {
+  admindash.style.display = "none";
+  sellerdash.style.display = "none";
+  let currentuser = localStorage.getItem("currentUser");
   currentuser = currentuser ? JSON.parse(currentuser) : false;
-  seller=currentuser.some(currentuser=>currentuser.role==="seller")
-  admin=currentuser.some(currentuser=>currentuser.role==="admin")
+  seller = currentuser.some((currentuser) => currentuser.role === "seller");
+  admin = currentuser.some((currentuser) => currentuser.role === "admin");
   if (currentuser && seller) {
-        sellerdash.style.display="block";
-        admindash.style.display="none";
+    sellerdash.style.display = "block";
+    admindash.style.display = "none";
+  } else if (currentuser && admin) {
+    admindash.style.display = "block";
+    sellerdash.style.display = "none";
   }
-  else if(currentuser && admin){
-        admindash.style.display="block";
-        sellerdash.style.display="none";
-  }
-
-        
-  
-
 }
