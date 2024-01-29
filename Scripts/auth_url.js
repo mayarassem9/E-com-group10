56e72afb1f98844f0d40a32ef39348d598144712
nadamohsen15 <eng.nadamohsen15@gmail.com>
2024-01-29 19:16:24 +0200

    push

--- Scripts/auth_url.js
+++ Scripts/auth_url.js
@@ -1,10 +1,11 @@
 let current = localStorage.getItem("currentUser");
-current = current ? JSON.parse(current) : false;
-let isseller=current.some((current) => current.role === "seller");
-let isadmin=current.some((current) => current.role === "admin");
+current = current ? JSON.parse(current):false;
+
 
 
 if(current){
+    let isseller=current.some((current) => current.role === "seller");
+    let isadmin=current.some((current) => current.role === "admin");
     if(!isadmin){
         window.location.href="index.html";
     }
